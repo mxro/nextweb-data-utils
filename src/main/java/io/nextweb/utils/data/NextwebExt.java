1191556755011824ba37c20dc87d11aca6a18a24
mxro <mxro@nowhere.com>
2014-11-17 08:37:57 +1100

    Implementing simple data utilities Autocommit Mon Nov 17 08:37:57 EST 2014

--- src/main/java/io/nextweb/utils/data/NextwebExt.java
+++ src/main/java/io/nextweb/utils/data/NextwebExt.java
@@ -1,5 +1,10 @@
 package io.nextweb.utils.data;
 
+import io.nextweb.Entity;
+
 @SuppressWarnings("all")
 public class NextwebExt {
+  public static Object removeRecursive(final Entity from, final Entity entity) {
+    return null;
+  }
 }
--- src/main/xtend/io/nextweb/utils/data/NextwebExt.xtend
+++ src/main/xtend/io/nextweb/utils/data/NextwebExt.xtend
@@ -1,5 +1,11 @@
 package io.nextweb.utils.data
 
+import io.nextweb.Entity
+
 class NextwebExt {
+	
+	def static removeRecursive(Entity from, Entity entity) {
+		
+	}
 	
 }
