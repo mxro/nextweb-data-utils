66f12db8fa6fc04ab5edc7e30c75df0942d4d17d
mxro <mxro@nowhere.com>
2014-11-17 08:38:57 +1100

    Implementing simple data utilities Autocommit Mon Nov 17 08:38:57 EST 2014

--- src/main/java/io/nextweb/utils/data/NextwebExt.java
+++ src/main/java/io/nextweb/utils/data/NextwebExt.java
@@ -4,6 +4,9 @@ import io.nextweb.Entity;
 
 @SuppressWarnings("all")
 public class NextwebExt {
+  /**
+   * Removes the specified node from the defined <code>form</code> node and all its <b>direct</b> children.
+   */
   public static Object removeRecursive(final Entity from, final Entity entity) {
     return null;
   }
--- src/main/xtend/io/nextweb/utils/data/NextwebExt.xtend
+++ src/main/xtend/io/nextweb/utils/data/NextwebExt.xtend
@@ -4,6 +4,9 @@ import io.nextweb.Entity
 
 class NextwebExt {
 	
+	/**
+	 * Removes the specified node from the defined <code>form</code> node and all its <b>direct</b> children.
+	 */
 	def static removeRecursive(Entity from, Entity entity) {
 		
 	}
