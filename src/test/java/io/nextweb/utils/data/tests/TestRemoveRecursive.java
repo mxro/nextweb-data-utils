abf2239c7c04b510733f11a72e82dcb463b2d2ca
mxro <mxro@nowhere.com>
2015-01-11 11:44:10 +1100

    Moving promise implementation into seperate project Autocommit Sun Jan 11 11:44:10 EST 2015

--- src/test/java/io/nextweb/utils/data/tests/TestRemoveRecursive.java
+++ src/test/java/io/nextweb/utils/data/tests/TestRemoveRecursive.java
@@ -1,21 +1,6 @@
 package io.nextweb.utils.data.tests;
 
-import com.appjangle.jre.AppjangleJre;
-import de.mxro.async.Async;
-import de.mxro.async.Deferred;
-import de.mxro.async.callbacks.ValueCallback;
-import de.mxro.async.jre.AsyncJre;
-import de.mxro.fn.Closure;
-import de.mxro.fn.Success;
-import de.mxro.tree.Tree;
 import de.oehme.xtend.junit.JUnit;
-import io.nextweb.Link;
-import io.nextweb.ListQuery;
-import io.nextweb.NodeList;
-import io.nextweb.Query;
-import io.nextweb.Session;
-import io.nextweb.common.LocalServer;
-import io.nextweb.promise.NextwebPromise;
 import io.nextweb.utils.data.NextwebDataExtension;
 import org.eclipse.xtext.xbase.lib.Extension;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure0;
@@ -29,48 +14,12 @@ import org.junit.internal.ArrayComparisonFailure;
 public class TestRemoveRecursive {
   @Test
   public void test() {
-    final LocalServer server = AppjangleJre.startServer();
-    final Session session = AppjangleJre.createSession(server);
-    final Query root = session.seed(server);
-    final Query node1 = root.append("node1", "./node1");
-    Query _append = node1.append("a child");
-    _append.append("and another");
-    Query _append_1 = node1.append("a sibling");
-    _append_1.append("and something");
-    final Query child3 = node1.append("child3");
-    Query _append_2 = child3.append("a");
-    _append_2.append("b");
-    child3.append("c");
-    NextwebPromise<Success> _commit = session.commit();
-    _commit.get();
-    final Deferred<Object> _function = new Deferred<Object>() {
-      public void apply(final ValueCallback<Object> cb) {
-        final Closure<Tree<Link>> _function = new Closure<Tree<Link>>() {
-          public void apply(final Tree<Link> tree) {
-            cb.onSuccess(Success.INSTANCE);
-          }
-        };
-        ValueCallback<Tree<Link>> _embed = Async.<Tree<Link>>embed(cb, _function);
-        TestRemoveRecursive.this.ext.collectDirectChildren(root, _embed);
-      }
-    };
-    AsyncJre.<Object>waitFor(_function);
-    final Deferred<Success> _function_1 = new Deferred<Success>() {
-      public void apply(final ValueCallback<Success> cb) {
-        TestRemoveRecursive.this.ext.removeRecursive(root, node1, cb);
-      }
-    };
-    AsyncJre.<Success>waitFor(_function_1);
-    NextwebPromise<Success> _commit_1 = session.commit();
-    _commit_1.get();
-    ListQuery _selectAll = node1.selectAll();
-    NodeList _get = _selectAll.get();
-    int _size = _get.size();
-    TestRemoveRecursive.<Integer, Integer>operator_doubleArrow(Integer.valueOf(_size), Integer.valueOf(0));
-    NextwebPromise<Success> _close = session.close();
-    _close.get();
-    NextwebPromise<Success> _shutdown = server.shutdown();
-    _shutdown.get();
+    throw new Error("Unresolved compilation problems:"
+      + "\nType mismatch: type Success is not applicable at this location"
+      + "\nType mismatch: cannot convert from (ValueCallback<?>)=>void to Operation<Object>"
+      + "\nType mismatch: cannot convert from (ValueCallback<Success>)=>void to Operation<Object>"
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context."
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context.");
   }
   
   @Extension
--- src/test/java/io/nextweb/utils/data/tests/TestRemoveRecursiveSafe.java
+++ src/test/java/io/nextweb/utils/data/tests/TestRemoveRecursiveSafe.java
@@ -1,21 +1,7 @@
 package io.nextweb.utils.data.tests;
 
-import com.appjangle.jre.AppjangleJre;
-import de.mxro.async.Async;
-import de.mxro.async.Deferred;
-import de.mxro.async.callbacks.ValueCallback;
-import de.mxro.async.jre.AsyncJre;
-import de.mxro.fn.Closure;
-import de.mxro.fn.Success;
 import de.oehme.xtend.junit.JUnit;
-import io.nextweb.ListQuery;
-import io.nextweb.NodeList;
-import io.nextweb.Query;
-import io.nextweb.Session;
-import io.nextweb.common.LocalServer;
-import io.nextweb.promise.NextwebPromise;
 import io.nextweb.utils.data.NextwebDataExtension;
-import java.util.List;
 import org.eclipse.xtext.xbase.lib.Extension;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure0;
 import org.hamcrest.Matcher;
@@ -28,58 +14,10 @@ import org.junit.internal.ArrayComparisonFailure;
 public class TestRemoveRecursiveSafe {
   @Test
   public void test() {
-    final LocalServer server = AppjangleJre.startServer();
-    final Session session = AppjangleJre.createSession(server);
-    final Query root = session.seed(server);
-    final Query node1 = root.append("node1", "./node1");
-    Query _append = node1.append("a child");
-    _append.append("and another");
-    Query _append_1 = node1.append("a sibling");
-    _append_1.append("and something");
-    final Query child3 = node1.append("child3");
-    Query _append_2 = child3.append("a");
-    _append_2.append("b");
-    child3.append("c");
-    NextwebPromise<Success> _commit = session.commit();
-    _commit.get();
-    ListQuery _selectAll = root.selectAll();
-    NodeList _get = _selectAll.get();
-    List<Object> _values = _get.values();
-    boolean _contains = _values.contains("node1");
-    TestRemoveRecursiveSafe.<Boolean, Boolean>operator_doubleArrow(Boolean.valueOf(_contains), Boolean.valueOf(true));
-    final Deferred<Object> _function = new Deferred<Object>() {
-      public void apply(final ValueCallback<Object> cb) {
-        final Closure<List<NextwebPromise<Success>>> _function = new Closure<List<NextwebPromise<Success>>>() {
-          public void apply(final List<NextwebPromise<Success>> qries) {
-            int _size = qries.size();
-            boolean _greaterThan = (_size > 0);
-            TestRemoveRecursiveSafe.<Boolean, Boolean>operator_doubleArrow(Boolean.valueOf(_greaterThan), Boolean.valueOf(true));
-            for (final NextwebPromise<Success> qry : qries) {
-              final Closure<Success> _function = new Closure<Success>() {
-                public void apply(final Success it) {
-                }
-              };
-              qry.get(_function);
-            }
-            cb.onSuccess(Success.INSTANCE);
-          }
-        };
-        ValueCallback<List<NextwebPromise<Success>>> _embed = Async.<List<NextwebPromise<Success>>>embed(cb, _function);
-        TestRemoveRecursiveSafe.this.ext.removeSafeRecursive(root, node1, _embed);
-      }
-    };
-    AsyncJre.<Object>waitFor(_function);
-    NextwebPromise<Success> _commit_1 = session.commit();
-    _commit_1.get();
-    ListQuery _selectAll_1 = root.selectAll();
-    NodeList _get_1 = _selectAll_1.get();
-    List<Object> _values_1 = _get_1.values();
-    boolean _contains_1 = _values_1.contains("node1");
-    TestRemoveRecursiveSafe.<Boolean, Boolean>operator_doubleArrow(Boolean.valueOf(_contains_1), Boolean.valueOf(false));
-    NextwebPromise<Success> _close = session.close();
-    _close.get();
-    NextwebPromise<Success> _shutdown = server.shutdown();
-    _shutdown.get();
+    throw new Error("Unresolved compilation problems:"
+      + "\nType mismatch: type Success is not applicable at this location"
+      + "\nType mismatch: cannot convert from (ValueCallback<?>)=>void to Operation<Object>"
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context.");
   }
   
   @Extension
